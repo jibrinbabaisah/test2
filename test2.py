a1e793e2a5b65b52e33051aef4a5bb12ab1ae5b2
jibrin <jibrinbabaisah@gmail.com>
2019-09-16 13:15:10 +0100

    first commit

---
+++ test2.py
@@ -0,0 +1,13 @@
+print("My Name is jibrin baba isah")
+print("this is a second test file")
+
+print(27/9)
+print(3**3)
+
+name = "John"
+age = 15
+
+#print("Hello! My name is %s" %name) 
+print("Hello! My name is %s and i am %d years old" % (name,age))
+
+
